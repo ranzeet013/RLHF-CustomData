8304b2dbb870f959ab0c7aad3d143460eb9dad33
ranzeet013 <ranzeetraut00@gmail.com>
2025-03-20 20:27:29 +0545

    created model.py

---
+++ SFT/flanT5/model.py
@@ -0,0 +1,6 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+    model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
+    return tokenizer, model
