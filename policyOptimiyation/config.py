b3297c3598ec638be4628579997a6aaa81a782f4
ranzeet013 <ranzeetraut00@gmail.com>
2025-03-23 20:12:21 +0545

    creaed config.py file

---
+++ policyOptimiyation/config.py
@@ -0,0 +1,11 @@
+# Hyperparameters
+LEARNING_RATE = 1e-5
+GAMMA = 0.99
+CLIP_EPSILON = 0.2
+EPOCHS = 3
+BATCH_SIZE = 8
+
+# Paths to your custom models
+SFT_MODEL_PATH = "sft_model"  
+REWARD_MODEL_PATH = "reward_model"  
+OPTIMIZED_MODEL_PATH = "optimized_policy_model"  
